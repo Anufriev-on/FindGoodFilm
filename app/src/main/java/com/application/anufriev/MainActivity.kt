8ca2e8234ee2edca699128d2b5a05f502b5bdbc0
Anufriev_on <Anufriev_on@mail.ru>
2022-01-16 01:05:00 +0700

    Module 47 was done

--- app/src/main/java/com/application/anufriev/MainActivity.kt
+++ app/src/main/java/com/application/anufriev/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.application.anufriev
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.application.anufriev.data.Entity.Film
 import com.application.anufriev.databinding.ActivityMainBinding
+import com.application.anufriev.receivers.ConnectionChecker
 import com.application.anufriev.view.fragments.*
 //import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver: BroadcastReceiver
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +37,19 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(null)
             .commit()
 
+        receiver = ConnectionChecker()
+        val filters = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filters)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
+
 
     fun launchDetailsFragment(film: Film) {
         //Создаем "посылку"
